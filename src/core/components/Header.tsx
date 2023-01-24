3bbb54eab9d88d201fbf8610d3a8e369a526c216
Anthony <anthonycolas.france@gmail.com>
2023-01-24 22:48:51 +0100

    Feature: Header style and phone icon OK

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -1,18 +1,18 @@
-import CallIcon from '@mui/icons-material/Call';
 import { Stack, Typography } from "@mui/material";
 import { Box } from '@mui/system';
 
 import logoSrc from "../../assets/images/logo.png";
+import PhoneIcon from "../../assets/images/phone.png";
 
 const Header = () => (
   <Box sx={{ position: "relative" }}>
     <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ zIndex: 2 }}>
       <img src={logoSrc} style={{ paddingTop: 1, paddingBottom: 2 }} alt="logo" />
       <Stack direction="row" alignItems="center">
-        <CallIcon sx={{ right: 0 }} />
-        <Stack direction="column" alignItems="flex-start">
-          <Typography variant='h6' align='right' sx={{}}>Contact Us</Typography>
-          <Typography variant='h5' align='right' sx={{}}>1-866-478-4119</Typography>
+      <img src={PhoneIcon} style={{ right: 0, height: '50px', width: '50px'}} alt="phone" />
+        <Stack direction="column" alignItems="flex-start" sx={{marginLeft: 2}}>
+          <Typography variant='h6' align='right' sx={{color: 'grey'}}>Contact Us</Typography>
+          <Typography variant='h5' align='right' sx={{fontWeight: 'bold'}}>1-866-478-4119</Typography>
         </Stack>
       </Stack>
     </Stack>
